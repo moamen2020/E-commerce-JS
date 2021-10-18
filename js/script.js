fe82be101cd1186080bb416a15a142b303954149
moamen2020 <m01144944981@gmail.com>
2021-10-18 16:34:33 -0400

    Add Cart Item and added Items Automatic

--- js/script.js
+++ js/script.js
@@ -18,3 +18,87 @@ logoutBtn.addEventListener("click", function () {
     window.location = 'register.html'
   }, 1500);
 })
+
+
+// define Products
+
+let prductsDom = document.querySelector('.products');
+let cartProductItem = document.querySelector(".carts-products");
+let cartProductDiv = document.querySelector(".carts-products div");
+let badgeCart = document.querySelector('.badge');
+let shoppingCartIcon = document.querySelector('.shoppingCart');
+
+let products = [
+  {
+    id: 1,
+    title: 'HeadPhone',
+    imageURL: 'images/headphone.jpg',
+    size: 'large'
+  },
+  {
+    id: 2,
+    title: 'HeadPhone',
+    imageURL: 'images/headphone.jpg',
+    size: 'large'
+  },
+  {
+    id: 3,
+    title: 'HeadPhone',
+    imageURL: 'images/headphone.jpg',
+    size: 'large'
+  },
+  {
+    id: 4,
+    title: 'HeadPhone',
+    imageURL: 'images/headphone.jpg',
+    size: 'large'
+  },
+]
+
+shoppingCartIcon.addEventListener("click", openCartMenu)
+
+
+function drowProductsUI() {
+  let productsUI = products.map((item) => {
+    return `
+    <div class="product-item">
+      <img src="${item.imageURL}" class="product-item-img" alt="HeadPhone Item">
+      <div class="product-item-des">
+        <h2>${item.title}</h2>
+        <p>Lorem ipsum dolor sit amet consectetur</p>
+        <span>Size : ${item.size}</span>
+      </div>
+      <div class="product-item-actions">
+        <button class="add-to-cart" onclick="addedToCart(${item.id})">Add To Cart</button>
+        <i class="favorite far fa-heart"></i>
+      </div>
+    </div>
+    `
+  }).join('')
+
+  prductsDom.innerHTML = productsUI
+}
+
+
+function addedToCart(id) {
+  let choosenItem = products.find( (item) => item.id === id )
+  cartProductDiv.innerHTML += `<p>${choosenItem.title}</p>`
+
+  let productsLength = document.querySelectorAll('.carts-products div p');
+  console.log(productsLength)
+  badgeCart.style.display = 'block';
+  badgeCart.innerHTML = productsLength.length
+}
+
+drowProductsUI();
+
+
+function openCartMenu() {
+  if (cartProductDiv.innerHTML != "") {
+    if (cartProductItem.style.display != 'block') {
+      cartProductItem.style.display =  'block'
+    } else {
+      cartProductItem.style.display =  'none'
+    }
+  }
+}
